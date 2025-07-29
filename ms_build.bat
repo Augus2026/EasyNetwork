bbb5fb83c0adcab26072a23fdccc778eef1fd790
shiguoliang <shiguoliang19@outlook.com>
2025-07-30 00:04:42 +0800

    flutter应用添加管理员权限

--- ms_build.bat
+++ ms_build.bat
@@ -1,22 +1,22 @@
 @echo off
 
-@REM pushd .\EasyNetwork\libEasyNetwork
-@REM cmake -S ./ -B ./build -DCMAKE_TOOLCHAIN_FILE=C:/Users/shiguoliang/temp/vcpkg-export-20250726-155106/scripts/buildsystems/vcpkg.cmake
-@REM cmake --build ./build --config Release
-@REM popd
+pushd .\EasyNetwork\libEasyNetwork
+cmake -S ./ -B ./build -DCMAKE_TOOLCHAIN_FILE=C:/Users/shiguoliang/temp/vcpkg-export-20250726-155106/scripts/buildsystems/vcpkg.cmake
+cmake --build ./build --config Release
+popd
 
-@REM pushd .\EasyNetwork\flutter
-@REM @REM flutter pub run ffigen --config ffigen.yaml
-@REM @REM flutter pub get
-@REM flutter build windows --release
-@REM popd
+pushd .\EasyNetwork\flutter
+@REM flutter pub run ffigen --config ffigen.yaml
+@REM flutter pub get
+flutter build windows --release
+popd
 
 @REM pushd .\api_server
 @REM cargo clean
 @REM cargo build --release
 @REM popd
 
-pushd .\mtls_server
-cmake -S ./ -B ./build -DCMAKE_TOOLCHAIN_FILE=C:/Users/shiguoliang/temp/vcpkg-export-20250726-155106/scripts/buildsystems/vcpkg.cmake
-cmake --build ./build --config Release
-popd
+@REM pushd .\mtls_server
+@REM cmake -S ./ -B ./build -DCMAKE_TOOLCHAIN_FILE=C:/Users/shiguoliang/temp/vcpkg-export-20250726-155106/scripts/buildsystems/vcpkg.cmake
+@REM cmake --build ./build --config Release
+@REM popd
